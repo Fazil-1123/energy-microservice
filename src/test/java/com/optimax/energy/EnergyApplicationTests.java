dc0301dae7790ea331fbe0d3ebf1bbed5093d4e3
Fazil-1123 <152527893+Fazil-1123@users.noreply.github.com>
2025-10-30 12:56:48 +0100

    chore: initial repo setup

---
+++ src/test/java/com/optimax/energy/EnergyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optimax.energy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
